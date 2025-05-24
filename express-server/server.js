04c9d69ce50d812d968e2ef2d9d94f1091944fd7
jcalle19 <jcalle19@asu.edu>
2025-05-23 20:47:32 -0700

    Setup initial server skeleton files
    Added initial content for starting servers and sockets

--- express-server/server.js
+++ express-server/server.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const cors = require('cors');
+const io = require('socket.io')(http);
+
+const apiRoutes = require('./routes/api');
 const app = express();
 const PORT = 3000;
+
+app.use('/api', apiRoutes);
+app.use(cors());
+
+io.on('connection', function (socket) {
+    console.log('a user has connected');
+})
 
 app.listen(PORT, () => {
     console.log(`Server is listening at http://localhost:${PORT}`);
