579d6b5fb7cafe07e4638c74e73638f0a4ea6cff
jcalle19 <jcalle19@asu.edu>
2025-06-28 21:56:21 -0700

    Added part of the second menu, still needs tweaking
    Added part of the second menu, still needs tweaking

--- collab-app/app/components/chatBox.jsx
+++ collab-app/app/components/chatBox.jsx
@@ -7,6 +7,7 @@ import '../css/roomPage.css'
 
 const ChatBox = () => {
     const [chatMsg, updateMsg] = useState('');
+    const [expanded, changeSize] = useState(false);
     const { userObj, socketRef, roomUsersKeys, chatMessages, chatMessagesRef, addMessage } = useStateContext();
 
     const handleSubmit = () => {
@@ -14,26 +15,35 @@ const ChatBox = () => {
         updateMsg('');
     }
     
+    const handleToggleClick = (e) => {
+        changeSize(!expanded);
+    }
+
     return (
-    <div className='chat-window'>
-        <div id='users-drop-down'>
-            {roomUsersKeys.map((user) => (<div key={user.key}>{user.user}</div>))}
+        <>
+            <div id='chat-window-toggle' className={expanded ? 'chat-window chat-toggle-expanded' : 'chat-window'} onClick={handleToggleClick}></div>
+            <div className={expanded ? 'chat-window chat-window-expanded grid grid-rows-3 gap-1' : 'chat-window transparent grid grid-rows-3 gap-1'}>
+                <div id='users-drop-down'>
+                    {roomUsersKeys.map((user) => (<div key={user.key}>{user.user}</div>))}
+                </div>
+                <section className='row-start-2'>
+                <div id='msg-field'>
+                    {
+                        chatMessages.length > 0 ? chatMessages.map((chat) => (<Message key={chat.key} user={chat.username} message={chat.content}/>)) : ''
+                    }
+                </div>
+                <form id='msg-input'>
+                    <input
+                        type='text'
+                        value={chatMsg}
+                        onChange={(e)=>{updateMsg(e.target.value)}}
+                        placeholder='Enter message'
+                    />
+                </form>
+                <button id='msg-submit' onClick={handleSubmit}>Send</button>
+            </section>
         </div>
-        <div id='msg-field'>
-            {
-                chatMessages.length > 0 ? chatMessages.map((chat) => (<Message key={chat.key} user={chat.username} message={chat.content}/>)) : ''
-            }
-        </div>
-        <form>
-            <input
-                type='text'
-                value={chatMsg}
-                onChange={(e)=>{updateMsg(e.target.value)}}
-                placeholder='Enter message'
-            />
-        </form>
-        <button onClick={handleSubmit}>Send</button>
-    </div>
+    </>
   )
 }
 
